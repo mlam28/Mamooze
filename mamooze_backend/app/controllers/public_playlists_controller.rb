985ad82ecdb918674461b94ef135e0d458b88195
github username <github email address>
2019-08-21 18:08:05 -0400

    home button

--- mamooze_backend/app/controllers/public_playlists_controller.rb
+++ mamooze_backend/app/controllers/public_playlists_controller.rb
@@ -1,2 +1,15 @@
 class PublicPlaylistsController < ApplicationController
+    def index
+        publicPlay = PublicPlaylist.all 
+        render json: publicPlay.to_json(public_playlist_serializer)
+    end
+    private
+    def public_playlist_serializer
+        {:include => {
+            :songs => {
+                :except => [:created_at, :updated_at]}
+            },
+        :except => [:created_at, :updated_at]
+    }
+    end
 end
--- mamooze_backend/config/routes.rb
+++ mamooze_backend/config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  resources :song_public_playlists
-  resources :public_playlists
-  resources :song_playlists
-=======
   resources :song_playlists, only: [:index, :create, :show, :update]
   delete '/song_playlists', to: 'song_playlists#destroy'
->>>>>>> origin/deletebutt
   resources :user_playlists
   resources :songs
   resources :playlists
   resources :users
+  resources :public_playlists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
